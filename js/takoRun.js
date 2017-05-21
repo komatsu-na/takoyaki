cbcd108cda4ef6d89327aafb2efd466b4eba8812
Rina Izumi <rina_izumi@kronos-jp.net>
2017-05-21 16:38:18 +0900

    '全ての動作が正しく動く状態'

--- js/takoRun.js
+++ js/takoRun.js
@@ -1,18 +1,14 @@
-// タコを走らせる
 $(function(){
   $("#tako").animate({
     top: "20%"
   }, 2500).animate({
     top: "80%"
-    // botton: "10%"
   }, 2500);
-  setTimeout(arguments.callee, 1000);
+  setTimeout(arguments.callee, 5000);
 });
 
-// タコ焼きをとばす
 $(function() {
-  $("button").onClick(function() {
-    // 飛ばしたたこ焼きが消えるまでイベント無効
+  $("button").click(function() {
     if ($("#takoyaki").length < 1) {
       $("div").append("<img src='./image/takoyaki.gif' id='takoyaki'/>");
       var takoyaki = $("#takoyaki")
