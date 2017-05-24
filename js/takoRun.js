e96b0aba164b78c75bc729e001ff0e5afc4b93d3
大久保　弘望 <ookubohiromi@ookubohiromochi-no-MacBook-Pro.local>
2017-05-24 20:56:01 +0900

    takoyaki

--- js/takoRun.js
+++ js/takoRun.js
@@ -1,21 +1,24 @@
 $(function(){
-  $("#button").animate({
+  $("#tako").animate({
     top: "20%"
   }, 2500).animate({
     top: "80%"
   }, 2500);
+  setTimeout(arguments.callee,5000);
 });
 
 $(function() {
-  $("button").onClick(function() {
+  $("button").Click(function() {
+    console.log("tako");
     if ($("#takoyaki").length < 1) {
-      $("div").append("<img src='./image/takoyaki.gif' id='takoyaki'/>");
+      $("#takoSpace").append("<img src='./image/takoyaki.gif' id='takoyaki'/>");
       var takoyaki = $("#takoyaki")
       takoyaki.css({
         "position": "absolute",
-        "top": $("#tako").css("top"),
-        "left": $("#tako").css("left")
+        "top": $("#takoSpace").css("top"),
+        "left": $("#takoSpace").css("left")
       });
+      takoyaki.animate({left:0},500);
       setTimeout(function() {
         takoyaki.remove();
       }, 500);
