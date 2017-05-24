1d2746a06f170cf027b8a12b7ec51927c661916f
mizuki_yamashita <mizuki_yamashita@kronos.jp>
2017-05-24 20:54:20 +0900

    たこ焼き飛びました

--- js/takoRun.js
+++ js/takoRun.js
@@ -1,13 +1,12 @@
 $(function(){
-  $("#button").animate({
+  $("#tako").animate({
     top: "20%"
-  }, 2500).animate({
-    top: "80%"
-  }, 2500);
+  }, 2500).animate({top: "80%"}, 2500);
+  setTimeout(arguments.callee, 5000);
 });
 
 $(function() {
-  $("button").onClick(function() {
+  $("#button").click(function() {
     if ($("#takoyaki").length < 1) {
       $("div").append("<img src='./image/takoyaki.gif' id='takoyaki'/>");
       var takoyaki = $("#takoyaki")
@@ -16,9 +15,10 @@ $(function() {
         "top": $("#tako").css("top"),
         "left": $("#tako").css("left")
       });
+      takoyaki.animate({left: "0%"}, 1000);
       setTimeout(function() {
         takoyaki.remove();
-      }, 500);
+      }, 1000);
     }
   });
 });
